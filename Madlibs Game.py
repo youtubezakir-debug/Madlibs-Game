90ca9e5120da00d38327014a6d1f71f80644e8e4
Zakir Hussain <youtubezakir@gmail.com>
2026-07-04 12:02:34 +0500

    Update Madlibs Game.py

--- Madlibs Game.py
+++ Madlibs Game.py
@@ -8,11 +8,15 @@ animal = input("Enter an animal: ")
 verb = input("Enter a verb ending with 'ing': ")
 food = input("Enter a food: ")
 
-print(f"Yesterday I visited a {adjective} {place}.")
-print(f"Suddenly, a giant {animal} came {verb} towards me.")
-print(f"Everyone started screaming and running away.")
-print(f"But the {animal} only wanted my {food}.")
-print(f"I shared my {food}, and we became best friends!")
-print("It was the funniest day of my life!")
+
+print(f"Yesterday, I & My friend's visited a {place}.")
+print(f"Out of nowhere, a giant {animal} appeared.")
+print(f"It started {verb}, and everyone was completely shocked.")
+print(f"I was nervous, but I stayed calm.")
+print(f"The {animal} only wanted to share my {food}.")
+print(f"We enjoyed the {food} together and became best friends!")
+print(f"Everyone cheered and took lots of pictures.")
+print("It turned out to be the funniest day of my life!")
+
 
 
