267ed64880f63bf2be78bc9b7f4c44ec04b91491
Zakir Hussain <youtubezakir@gmail.com>
2026-07-04 11:49:23 +0500

    Update Madlibs Game.py

--- Madlibs Game.py
+++ Madlibs Game.py
@@ -2,4 +2,17 @@
 # Word Game where you create Story
 # by filling in blanks with random words
 
+adjective = input("Enter an adjective (Description): ")
+place = input("Enter a place: ")
+animal = input("Enter an animal: ")
+verb = input("Enter a verb ending with 'ing': ")
+food = input("Enter a food: ")
+
+print(f"Yesterday I visited a {adjective} {place}.")
+print(f"Suddenly, a giant {animal} came {verb} towards me.")
+print(f"Everyone started screaming and running away.")
+print(f"But the {animal} only wanted my {food}.")
+print(f"I shared my {food}, and we became best friends!")
+print("It was the funniest day of my life!")
+
 
